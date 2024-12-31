d6538afbbd8f95c0ca295da120e43bf0fe2d29b5
Amulya <147150377+Amulya-sg@users.noreply.github.com>
2024-12-31 11:55:56 +0530

    Update App.java

--- src/main/java/com/app/demo/javamaven/App.java
+++ src/main/java/com/app/demo/javamaven/App.java
@@ -2,6 +2,6 @@ package com.app.demo.javamaven;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello kmit!");
+        System.out.println("Hello kmit");
     }
 }
